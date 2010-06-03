8e4f1d88165fa46db820854bb97648ce1945029e
Thomas Fach-Pedersen <thomas@fach-pedersen.net>
2010-06-04 00:43:33 +0200

    Remove test code

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 
 int main(int argc, char **argv)
 {
-	/*
 	if (argc != 2)
 	{
 		printf("Syntax: %s file.exe\n", argv[0]);
@@ -15,9 +14,6 @@ int main(int argc, char **argv)
 	}
 
 	raw_imstream_t is(argv[1]);
-	*/
-
-	raw_imstream_t is("/Users/madmoose/Development/bins/zak.exe");
 
 	binary_t *binary = new exe_mz_t();
 	binary->load(is);
@@ -31,26 +27,4 @@ int main(int argc, char **argv)
 
 	fmt_stream fs;
 	analyzer.output(fs);
-
-	//uint32 base = 0x1000;
-	//loader->load(is, base);
-
-	/*
-	puts("1");
-	fmt_stream os;
-	puts("2");
-
-	os.set_line_id("seg000:0001");
-	os.puts("; this is a test!");
-	os.puts("; Hallo? :)");
-
-	os.set_line_id("seg000:002e");
-	os.set_col(28);
-	os.printf("mov ax, %04x", 32);
-
-	os.set_col(52);
-	os.printf("; CODE XREF: ");
-
-	os.putchar('\n');
-	*/
 }
